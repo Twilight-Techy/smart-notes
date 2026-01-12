82fc72810517c992e0579d36153a7104fc5e4f74
Twilight Techie <da.kings.air@gmail.com>
2026-01-12 07:56:03 +0100

    fix: resolve tailwindcss dependency issue. Downgrade tailwind 4 to tailwind 3

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,15 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
-/** @type {import('expo/metro-config').MetroConfig} */
+// Get the default config from Expo
 const config = getDefaultConfig(__dirname);
 
+// Add SQL extension support for Drizzle migrations
 config.resolver.sourceExts.push('sql');
 
-module.exports = withNativeWind(config, { input: './global.css' });
+// Export with NativeWind wrapper, using safe absolute path for Windows
+module.exports = withNativeWind(config, {
+    input: path.resolve(__dirname, 'global.css'),
+    inlineRems: true
+});
